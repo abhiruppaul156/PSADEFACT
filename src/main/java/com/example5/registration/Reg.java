8b9175d3b8718fec8333afaacd65eddc6ae0ee5e
abhiruppaul156 <abhiruppaul156@gmail.com>
2025-02-13 21:51:08 +0530

    Defeact changes commited to the directory

--- src/main/java/com/example5/registration/Reg.java
+++ src/main/java/com/example5/registration/Reg.java
@@ -1,7 +1,8 @@
 package com.example5.registration;
 
 public class Reg {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("it is ");
     }
 }
