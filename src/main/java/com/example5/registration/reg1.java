fda430cbf9a27fab740efef04c925b7ee7312fa5
abhiruppaul156 <abhiruppaul156@gmail.com>
2025-02-13 22:09:00 +0530

    defeact 31423423

---
+++ src/main/java/com/example5/registration/reg1.java
@@ -0,0 +1,9 @@
+package com.example5.registration;
+
+public class reg1 {
+
+    public void test2()
+    {
+        System.out.println(300);
+    }
+}
---
+++ src/main/java/com/example5/registration/reg3.java
@@ -0,0 +1,9 @@
+package com.example5.registration;
+
+public class reg3
+{
+    public void test2()
+    {
+        System.out.println(450);
+    }
+}
